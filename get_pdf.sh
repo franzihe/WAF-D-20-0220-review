c57d9b46943b09298c7db2ce523d35c9a8824759
franzihe <franzihe@geo.uio.no>
2021-05-20 18:05:27 +0200

    changed naming conventions

--- get_pdf.sh
+++ get_pdf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 latexdiff --append-context2cmd="abstract" revision_V1.tex revision_V2.tex --graphics-markup=1 > diff.tex
 
-pdflatex diff.tex diff.pdf
+pdflatex diff_V1.tex diff_V1.pdf
 bibtex diff
 pdflatex diff.tex diff.pdf
 
